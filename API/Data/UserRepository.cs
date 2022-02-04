113f6c65590971fa907ed1d0298e3ec522ab26ac
Thyago <thyagopiske@gmail.com>
2022-02-04 13:18:13 -0300

    Section 18 - Unit Of Work and Finishing Touches

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -41,11 +41,6 @@ namespace API.Data
                 .SingleOrDefaultAsync(x => x.UserName == username);
         }
 
-        public async Task<bool> SaveAllAsync()
-        {
-            return await _context.SaveChangesAsync() > 0;
-        }
-
         public void Updtate(AppUser user)
         {
             _context.Entry(user).State = EntityState.Modified;
@@ -80,5 +75,13 @@ namespace API.Data
                     .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
                     .SingleOrDefaultAsync();
         }
+
+        public async Task<string> GetUserGender(string username)
+        {
+            return await _context.Users
+                .Where(x => x.UserName == username)
+                .Select(x => x.Gender)
+                .FirstOrDefaultAsync();
+        }
     }
-}
+} 
