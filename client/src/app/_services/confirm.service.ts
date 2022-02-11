e91ecc2b7ddb638a36b945e8753e4567717fec83
Thyago <thyagopiske@gmail.com>
2022-02-11 15:11:23 -0300

    Section 19 - pre switch to postgress

--- client/src/app/_services/confirm.service.ts
+++ client/src/app/_services/confirm.service.ts
@@ -34,7 +34,7 @@ export class ConfirmService {
         observer.next(this.bsModelRef.content.result);
         observer.complete();
       });
-
+      
       return{
         unsubscribe(){
           subscription.unsubscribe();
